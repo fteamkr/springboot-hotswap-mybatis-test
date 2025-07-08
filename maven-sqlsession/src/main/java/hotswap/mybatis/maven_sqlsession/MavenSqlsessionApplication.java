4773114095340cb0f51e2834e9dfa7ed0e15fd5b
Jaisung Lee <jaisung.lee@fteam.kr>
2025-07-08 19:55:06 +0900

    maven-sqlsession by start.spring.io

---
+++ maven-sqlsession/src/main/java/hotswap/mybatis/maven_sqlsession/MavenSqlsessionApplication.java
@@ -0,0 +1,13 @@
+package hotswap.mybatis.maven_sqlsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSqlsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSqlsessionApplication.class, args);
+	}
+
+}
---
+++ maven-sqlsession/src/test/java/hotswap/mybatis/maven_sqlsession/MavenSqlsessionApplicationTests.java
@@ -0,0 +1,13 @@
+package hotswap.mybatis.maven_sqlsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenSqlsessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
