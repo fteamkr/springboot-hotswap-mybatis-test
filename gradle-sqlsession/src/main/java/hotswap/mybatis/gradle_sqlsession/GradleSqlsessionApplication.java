26730a7acce411053267b887a0bae55c61e9a45e
Jaisung Lee <jaisung.lee@fteam.kr>
2025-07-08 19:55:20 +0900

    gradle-sqlsession by start.spring.io

---
+++ gradle-sqlsession/src/main/java/hotswap/mybatis/gradle_sqlsession/GradleSqlsessionApplication.java
@@ -0,0 +1,13 @@
+package hotswap.mybatis.gradle_sqlsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSqlsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSqlsessionApplication.class, args);
+	}
+
+}
---
+++ gradle-sqlsession/src/test/java/hotswap/mybatis/gradle_sqlsession/GradleSqlsessionApplicationTests.java
@@ -0,0 +1,13 @@
+package hotswap.mybatis.gradle_sqlsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleSqlsessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
